e6ee195fb3a0cc465166f3b69f5705b48c738946
Marcio Henriques <mhadsouza@gmail.com>
2023-11-07 19:38:59 -0300

    Add files via upload

---
+++ exercicio01_backend.py
@@ -0,0 +1,6 @@
+valorcompra = float(input ("Informe o valor total da compra:"))
+parcelas = int(input("Informe o numero de parcelas"))
+
+valorparcela = valorcompra / parcelas
+print (f"O valor da parcela é: {valorparcela:.2f}")
+
---
+++ exercicio02_backend.py
@@ -0,0 +1,5 @@
+celsius = float(input("Informe a temperatura em celsius: "))
+f = 9 * celsius / 5 + 32
+
+print (f"{celsius}ºC equivalem a {f:.2f}F")
+
